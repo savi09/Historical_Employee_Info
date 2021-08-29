d10e8352a733702f029248073b0f6e82d6be86be
Susana Villagrana <savi@ucdavis.edu>
2021-08-28 19:33:53 -0700

    Updates to SQL

--- Employee SQL Query.sql
+++ Employee SQL Query.sql
@@ -6,7 +6,7 @@ CREATE TABLE titles (
 CREATE TABLE salaries (
     emp_no INTEGER,
     salary INTEGER
-)
+);
 
 CREATE TABLE employees(
     emp_no  INTEGER,
@@ -15,19 +15,19 @@ CREATE TABLE employees(
     last_name VARCHAR,
     sex VARCHAR,
     hire_date DATE
-)
+);
 
 CREATE TABLE dept_manager (
     dept_no VARCHAR,
     emp_no INTEGER
-)
+);
 
 CREATE TABLE dept_emp (
     emp_no INTEGER,
     dept_no VARCHAR
-)
+);
 
 CREATE TABLE departments (
     dept_no VARCHAR,
     dept_name VARCHAR
-)  
+);
